b51c4df9ac302513de1519f54b146bbe059ab508
duyichen <duyichen007@gmail.com>
2013-09-02 07:46:49 +0800

    81

---
+++ LeetCode/src/LeetCode/CombinationSum.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+import java.util.ArrayList;
+
+public class CombinationSum {
+
+
+	public static void main(String[] args) {
+
+	}
+    public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
+
+    	ArrayList<ArrayList<Integer>>  comb=new ArrayList<ArrayList<Integer>>();
+    	return comb;
+        
+    }
+}
--- LeetCode/src/LeetCode/ReverseNodesinkGroup.java
+++ LeetCode/src/LeetCode/ReverseNodesinkGroup.java
@@ -4,26 +4,40 @@ public class ReverseNodesinkGroup {
 
     public ListNode reverseKGroup(ListNode head, int k) {
 
-    	if(head==null)
+    	if(head==null||k==1)
     		return head;
     		
-    	int i=1;
-    	ListNode end=head;
     	
-    	ListNode p=head.next;
-    	ListNode pre=head;
-    	ListNode next=null;
-    	while(i<k&&p!=null){
-    
-    		next=p.next;
-    		p.next=pre;
-    		pre=p;
-    		p=next;
-    		i++;
+    	ListNode first=new ListNode(0);
+    	first.next=head;
+    	ListNode pre=first;
+    	ListNode current=head;
+    	int count=k;
+    	while(current!=null){
+    		count--;
+    		ListNode  next=current.next;
+    		if(count==0){
+    			
+    			ListNode l1=pre.next;
+    			pre.next=current;
+    			pre=l1;
+    			ListNode l2=next;
+    			for(int i=0;i<k;i++){
+    				ListNode temp=l1.next;
+    				
+    				l1.next=l2;
+    				l2=l1;
+    				l1=temp;
+    			}
+    			count=k;
+    		}
+    		current=next;
     	}
-    	end.next=p;
     	
-        return pre;
+    	
+    
+    	
+        return first.next;
         
     }
 
