14d3c0caba313a4f627aba9cc2c216712e8de4c2
icelitchi <cduyi@L-SHC-00425986.corp.ebay.com>
2013-09-06 17:46:24 +0800

    -120

--- LeetCode/src/LeetCode/JumpGameII.java
+++ LeetCode/src/LeetCode/JumpGameII.java
@@ -9,11 +9,25 @@ public class JumpGameII {
 		// TODO Auto-generated method stub
 
 	}
+	//动态规划
     public int jump(int[] A) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-    	//TODO::
-        return 0;
+    int[] steps=new int[A.length];
+    //-1表示不可达
+    steps[0]=0;
+    for(int i=1;i<A.length;i++){
+    	int tempstep=Integer.MAX_VALUE;
+    	for(int j=0;j<i;j++){
+    		if(A[j]>=i-j&&A[j]>=0&&tempstep>A[j]+1){
+    			tempstep=steps[j]+1;
+    		}
+    	}
+    	if(tempstep==Integer.MAX_VALUE)
+    		steps[i]=-1;
+    	else
+    		steps[i]=tempstep;
+    			
+    }
+        return steps[A.length-1] ;
     }
 
 }
