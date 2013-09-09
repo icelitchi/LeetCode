025d5fae577c99d1430143eacb4995e2d854b304
icelitchi <cduyi@L-SHC-00425986.corp.ebay.com>
2013-09-09 18:01:10 +0800

    129

--- LeetCode/src/LeetCode/CombinationSum.java
+++ LeetCode/src/LeetCode/CombinationSum.java
@@ -5,8 +5,16 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-//可重复的背包问题
+//可重复的背包问题（不同，背包问题是有重量和价值的问题）
+//组合 和 问题
+
 //所选货物的值==target
+
+
+
+//DP动态规划，大数据集 超时,DP适合计算有几种情况
+//DFS 深度优先搜索
+//http://www.cnblogs.com/remlostime/archive/2012/10/29/2745083.html
 public class CombinationSum {
 
 
@@ -16,7 +24,31 @@ public class CombinationSum {
 		
 		test.combinationSum(c, 100);
 	}
-    public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
+	  public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
+			ArrayList<ArrayList<Integer>>  comb=new ArrayList<ArrayList<Integer>>();
+			combination(candidates,new int[candidates.length],0,target,comb);
+			return comb;
+	  }
+	  void combination(int[] can,int[] count,int dep,int target,ArrayList<ArrayList<Integer>>  comb){
+		  if(dep==can.length){
+			  if(target==0){
+				  ArrayList<Integer> res=new ArrayList<Integer>();
+				  for(int i=0;i<count.length;i++){
+					  for(int j=0;i<count[i];i++){
+						  res.add(can[i]);
+					  }
+				  }
+				  comb.add(res);
+			  }
+			  return ;
+		  }
+		  for(int i=0;i<=target/can[dep];i++){
+			  count[dep]=i;
+			  combination(can,count,dep+1,target-can[dep]*(i+1),comb);
+		  }
+	  }
+	  
+    public ArrayList<ArrayList<Integer>> _combinationSum(int[] candidates, int target) {
 
     	ArrayList<ArrayList<Integer>>  comb=new ArrayList<ArrayList<Integer>>();
     	Arrays.sort(candidates);
@@ -77,4 +109,8 @@ public class CombinationSum {
     	return comb;
         
     }
+    
+    
+    
+    
 }
--- LeetCode/src/LeetCode/CombinationSumII.java
+++ LeetCode/src/LeetCode/CombinationSumII.java
@@ -11,68 +11,43 @@ public class CombinationSumII {
 
 
 	}
+	// 候选 组合  和 为target
+	//侯选值 有重复
+//	For example, given candidate set 10,1,2,7,6,1,5 and target 8, 
+//	A solution set is: 
+//	[1, 7] 
+//	[1, 2, 5] 
+//	[2, 6] 
+//	[1, 1, 6] 
 	
-    public ArrayList<ArrayList<Integer>> combinationSum2(int[] candidates, int target) {
-    	ArrayList<ArrayList<Integer>>  comb=new ArrayList<ArrayList<Integer>>();
-    	Arrays.sort(candidates);
-    	int num=candidates.length;
-    	int sum=candidates[0];
-    	//存放 sum 和 sum 对应每个candidates个数的int[] list
-    	Map<Integer,ArrayList<int[]>> map=new HashMap<Integer,ArrayList<int[]>>();
-    	//初始化map
-    	for(int i=0;i<num;i++){
-    		ArrayList<int[]> c=new ArrayList<int[]>();
-    		int[] r=new int[num];
-    		r[i]=1;
-    		c.add(r);
-    		map.put(candidates[i], c);
-    	}
+	//DP 大数据超时
+	
+	//DFS 方法
+	 public ArrayList<ArrayList<Integer>> combinationSum2(int[] candidates, int target) {
+		 ArrayList<ArrayList<Integer>>  comb=new ArrayList<ArrayList<Integer>>(); 
+		 Arrays.sort(candidates);
+		 HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();// candidate-index
+		 ArrayList<Integer> can=new ArrayList<Integer>();//candidate不含重复的，拍好序的
+		 ArrayList<Integer> count=new ArrayList<Integer>();//candidate 对应数量
+		 int index=0;
+		 
+		 for(int i=0;i<candidates.length;i++){
+			 if(map.get(candidates[i])==null){
+				 map.put(candidates[i], index);
+				 count.add(index,1);
+				 can.add(candidates[i]);
+				 index++;
+			 }else{
+				 count.set(map.get(candidates[i]), count.get(map.get(candidates[i]))+1);
+			 }
+		 }
+		 combination(can,count,new int[can.size()],0,comb);
+		 return comb;
+	 }
+	
+	void combination(ArrayList<Integer> can,ArrayList<Integer> count,int[] num,int dep, ArrayList<ArrayList<Integer>> comb){
+		
+	}
 
-    	while(sum<=target){
-    		for(int i=0;i<num;i++){
-    			int remainder=sum-candidates[i];
-    			if(remainder<=0){
-    				break;
-    			}else{
-    				ArrayList<int[]> r1=map.get(remainder);
-    				if(r1!=null){
-    					ArrayList<int[]> r=map.get(sum);
-    					if(r==null)
-    						r=new ArrayList<int[]>();
-    					for(int j=0;j<r.size();j++){
-    						int[] c=Arrays.copyOf(r.get(i), num);
-    						if(c[i]==0){
-    							c[i]++;
-    							r.add(c);
-    						}
-    						
-    						
-    					}
-    					if(r.size()>0)
-    						map.put(sum, r);
-    						
-    				}
-    			}
-    		}
-    	}
-    	
-    	ArrayList<int[]> r=map.get(target);
-    	if(r!=null){
-    		for(int i=0;i<r.size();i++){
-    			int[] c=r.get(i);
-    			ArrayList<Integer> e=new ArrayList<Integer>();
-    			for(int j=0;j<num;j++){
-    			
-    					while(c[j]>0){
-    						e.add(candidates[j]);
-    						c[j]--;
-    					}
-    			}
-    			comb.add(e);
-    		}
-    	}
-    	return comb;
-        
-    }
-
+    
 }
